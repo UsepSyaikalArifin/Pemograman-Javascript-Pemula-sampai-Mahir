a2e716ccde5aa52b979549bd757fa362e1a8f452
InfiniteExplore <useparifinsyaikal@gmail.com>
2023-08-02 07:18:33 +0700

    Finish learning String Template Materi

--- 53 - String Template/script.js
+++ 53 - String Template/script.js
@@ -1,4 +1,12 @@
 const name = `Usep Syaikal Arifin`;
 const template = `Name : ${name}`;
 
+const nilai = 70;
+const template2 = `Name: ${name} Status: ${nilai > 60}`;
+const multiLine = `Nama saya Usep Syaikal Arifn
+Sering juga bikin konten programer
+kalau lagi malas paling tidur`;
+
 console.log(template);
+console.log(template2);
+document.writeln(`<pre>${multiLine}</pre>`);
