2a440adc9e8d9590fd326825a3179c49eb849363
InfiniteExplore <useparifinsyaikal@gmail.com>
2023-08-02 18:34:41 +0700

    Finish Type Data Array Material

---
+++ 55 - Tipe Data Array/script.js
@@ -0,0 +1,17 @@
+const arrayKosong = [];
+const arrayNama = ["Usep", "Syaikal", "Arifn"];
+
+const nama = [];
+nama.push("Usep");
+nama.push("Syaikal");
+
+console.table(arrayKosong);
+console.table(arrayNama);
+console.table(nama);
+nama[0] = "Budi";
+nama[1] = "Programmer";
+console.table(nama);
+nama.push(1, 2, 3, 4, 5);
+console.table(nama);
+nama.push(["Usep", "Syaikal", "Arifin"]);
+console.table(nama);
