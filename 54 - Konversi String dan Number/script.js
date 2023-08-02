3d6889c341e89c19d663036a63efd0a8b28ab28e
InfiniteExplore <useparifinsyaikal@gmail.com>
2023-08-02 07:28:47 +0700

    Initial Commit + Koversi String dan Number

---
+++ 54 - Konversi String dan Number/script.js
@@ -0,0 +1,7 @@
+const value1 = "1";
+const value2 = 1;
+
+console.log(`The result : ${Number(value1) + value2}`);
+console.log(`${parseInt("1.1")}`);
+console.log(`${parseFloat("1.1")}`);
+console.log(`${Number("1.1")}`);
