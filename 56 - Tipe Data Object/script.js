5cc19d385731c3b7148101b6d2fea9af4d1e41d5
InfiniteExplore <useparifinsyaikal@gmail.com>
2023-08-02 19:02:53 +0700

    Finish Type Object Data Material

---
+++ 56 - Tipe Data Object/script.js
@@ -0,0 +1,15 @@
+const person = {};
+person["nama"] = "Usep Syaikal Arifin";
+person["pekerjaan"] = "ReactJs Developers";
+person["umur"] = 26;
+
+const person2 = {
+  nama: "Usep Syaikal Arifin, S.T.",
+  pekerjaan: "ReactJs Developer",
+  umur: 26,
+};
+
+console.log(person.nama);
+
+console.table(person);
+console.table(person2);
