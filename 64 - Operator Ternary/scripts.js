a2333494dd940a643cd0c735a17610a7a72835cd
InfiniteExplore <useparifinsyaikal@gmail.com>
2023-08-03 18:56:19 +0700

    Finish Ternary Operator Material

---
+++ 64 - Operator Ternary/scripts.js
@@ -0,0 +1,14 @@
+const nilai = 75;
+let ucapan;
+
+if (nilai >= 75) {
+  ucapan = "Selamat Anda Berfikir";
+} else {
+  ucapan = "Tidak Selamat Ah Anda kurang berfikir";
+}
+
+console.log(ucapan);
+
+const nilai2 = 80;
+const ucapan2 = nilai2 >= 80 ? "Selamat Anda Lulus" : "Anda Tidak Lulus";
+console.log(ucapan2);
